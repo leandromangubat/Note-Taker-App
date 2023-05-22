c1c640cc2b7f9a7457f655ece27f846c8575d112
Leandro Mangubat <leandromangubat@hotmail.com>
2023-05-22 18:24:57 -0400

    update routes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const htmlRoutes = require('./routes/html_routes');
 const apiRoutes = require('./routes/api_routes');
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
