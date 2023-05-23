edbf0d9c6ce6abd217da4efe83c8aa8ca86ced37
Leandro Mangubat <leandromangubat@hotmail.com>
2023-05-23 18:21:44 -0400

    fix file name

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../develop/public/index.html"))
+    res.sendFile(path.join(__dirname, "../Develop/public/index.html"))
 });
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../develop/public/notes.html'))
+    res.sendFile(path.join(__dirname, '../Develop/public/notes.html'))
 });
 
 module.exports = router;
