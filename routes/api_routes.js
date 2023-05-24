96cc329394a326314d320fa7b13e394f5dfc2d0f
Leandro Mangubat <leandromangubat@hotmail.com>
2023-05-23 22:11:02 -0400

    shortened method code

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,5 +1,5 @@
 const note = require('express').Router();
-const fs = require('fs');
+const {readFileSync, writeFileSync} = require('fs');
 //Generates uuid for each note
 const uuid = () => {
     return Math.floor((1 + Math.random()) * 0x10000)
@@ -9,13 +9,13 @@ const uuid = () => {
 
 // GET method
 note.get('/api/notes', (req, res) => {
-    const jsonNote = JSON.parse(fs.readFileSync("Develop/db/db.json", "utf8"));
+    const jsonNote = JSON.parse(readFileSync("Develop/db/db.json", "utf8"));
     res.json(jsonNote);
 });
 
 //POST method
 note.post('/api/notes', (req, res) => {
-    const jsonNote = JSON.parse(fs.readFileSync("Develop/db/db.json", "utf8"));
+    const jsonNote = JSON.parse(readFileSync("Develop/db/db.json", "utf8"));
     const {title, text} = req.body;
     const newNote = {
         title,
@@ -23,19 +23,19 @@ note.post('/api/notes', (req, res) => {
         id: uuid(),
     }
     jsonNote.push(newNote);
-    fs.writeFileSync("Develop/db/db.json", JSON.stringify(jsonNote));
+    writeFileSync("Develop/db/db.json", JSON.stringify(jsonNote));
     res.json(jsonNote);
 });
 
 //DELETE method
 note.delete('/api/notes/:id', (req, res) => {
-    const jsonNote = JSON.parse(fs.readFileSync("Develop/db/db.json", "utf8"));
+    const jsonNote = JSON.parse(readFileSync("Develop/db/db.json", "utf8"));
     //Filters note to be deleted
     const delNote = jsonNote.filter((note) => {
         return note.id !==req.params.id;
     });
     //Creates updated file with deleted note
-    fs.writeFileSync("Develop/db/db.json", JSON.stringify(delNote));
+    writeFileSync("Develop/db/db.json", JSON.stringify(delNote));
     res.json("Note Deleted");
 });
 
